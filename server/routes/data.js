0c80a4a0952a2bee61b8d154f9f05f3c55c58373
peng <1024371442@qq.com>
2016-11-17 08:59:06 +0800

    首次提交

---
+++ server/routes/data.js
@@ -0,0 +1,142 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var PATH = 'public/data/';
+
+ // 读取数据模块 ——客户端调用
+// data/read?type=it
+router.get('/read', function(req, res, next) {
+    var type = req.param('type') || "";
+    fs.readFile(PATH + type + '.json', function (err, data){
+       if(err){
+           return res.send({
+               status:0,
+               info:'读取文件异常'
+           });
+       }
+       var COUNT = 50;
+        // TODO: try{}catch(){}
+        var obj =[];
+        try{
+            obj = JSON.parse(data.toString());
+        }catch(e){
+            obj = [];
+        }
+        if(obj.length > COUNT){
+            obj = obj.slice(0,COUNT);
+        }
+        return res.send({
+            status:1,
+            data:obj
+        })
+    });
+});
+
+// 数据存储模块——后台开发使用
+router.post('/write',function(req, res, next){
+    // 文件名
+    var type = req.param('type') || "";
+    // 关键字段
+    var url = req.param('url') || '';
+    var title = req.param('title') || '';
+    var img = req.param('img') || '';
+    if(!type || !url || !title || !img){
+        return res.send({
+            status:0,
+            info:'提交信息不完整'
+        })
+    }
+    // 1)需要拿到文件的信息
+    var filepath = PATH + type + '.json';
+    fs.readFile(filepath,function(err, data){
+        if(err){
+            return res.send({
+                status:0,
+                info:'读取文件失败'
+            })
+        }
+        var arr = JSON.parse(data.toString());
+        var obj = {
+            img: img,
+            url: url,
+            id: guidGenerate(),
+            time: new Date()
+        };
+        arr.splice(0,0,obj);
+        // 2)写入文件
+        var newData = JSON.stringify(arr);
+        fs.writeFile(filepath, newData, function(err, data){
+            if(err){
+                return res.send({
+                    status:0,
+                    info:'写入文件失败'
+                })
+            }
+            return res.send({
+                status:1,
+                data:newData
+            })
+        })
+    });
+
+
+
+});
+
+// 阅读模块写入接口 ——后台开发使用
+router.post('write_config', function(req, res, next){
+    //TODO:后期进行提交数据的验证
+    // 防xss攻击
+    // cnpm install
+    // var xss = require('xss')
+    // var str = xss(name)
+    var data = req.body.data;
+    var obj = JSON.parse(data);
+    var newData = JSON.stringify(obj);
+    // 写入
+    fs.writeFile(PATH + 'config.json',newData, function(err, data){
+        if(err){
+            return res.send({
+                status:0,
+                info:'读取文件失败'
+            })
+        }
+        return res.send({
+            status:1,
+            info:'数据写入成功',
+            data:newData
+        })
+    })
+});
+
+// 登录接口
+router.post('/login',function(req, res, next){
+    // 用户名、密码
+    var username = req.body.username;
+    var password = req.body.password;
+    //TODO:对用户名、密码进行校验
+    // 密码加密 md5(password + '随机字符串')
+    if(username === 'admin' && password === '123456'){
+        req.session.user = {
+            username:username
+        };
+        return res.send({
+            status:1,
+            info:'登录成功'
+        });
+    }
+    return res.send({
+        status:0,
+        info:'登录失败'
+    })
+});
+
+function guidGenerate() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    }).toUpperCase();
+}
+
+module.exports = router;
